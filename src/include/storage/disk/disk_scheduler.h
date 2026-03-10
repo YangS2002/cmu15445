afa4affd0da30922d6687e3e564cb9393d4a4040
yangs <2660089305@qq.com>
2026-03-10 17:18:59 +0800

    p1-task2-线程安全未测试-提交

--- src/include/storage/disk/disk_scheduler.h
+++ src/include/storage/disk/disk_scheduler.h
@@ -81,7 +81,9 @@ class DiskScheduler {
    *
    * @return std::promise<bool>
    */
-  auto CreatePromise() -> DiskSchedulerPromise { return {}; };
+  auto CreatePromise() -> DiskSchedulerPromise {
+     return DiskSchedulerPromise{}; 
+     };
 
   /**
    * @brief Increases the size of the database file to fit the specified number of pages.
--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "storage/disk/disk_scheduler.h"
+#include <optional>
+#include <utility>
 #include "common/exception.h"
 #include "storage/disk/disk_manager.h"
 
@@ -18,9 +20,9 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
+  // throw NotImplementedException(
+  //     "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+  //     "throw exception line in `disk_scheduler.cpp`.");
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
@@ -34,8 +36,31 @@ DiskScheduler::~DiskScheduler() {
   }
 }
 
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
 
-void DiskScheduler::StartWorkerThread() {}
+  request_queue_.Put(std::move(r));
+
+}
+
+void DiskScheduler::StartWorkerThread() {
+  while(true){
+    auto req = request_queue_.Get();
+    if(req.has_value()) {
+      // handle request
+      if(req->is_write_){
+        disk_manager_->WritePage(req->page_id_, req->data_);
+      }
+      else{
+        disk_manager_->ReadPage(req->page_id_,req->data_);
+      }
+      // signal completion
+      req->callback_.set_value(true);
+    }
+    else{
+      // nullopt
+      return;
+    }
+  }
+}
 
 }  // namespace bustub
